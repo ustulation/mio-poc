350f283ab37f511b5f53783860e02007ae1b78b6
ustulation <ustulation@gmail.com>
2017-02-01 00:49:22 +0000

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,197 @@
+extern crate byteorder;
+extern crate mio;
+extern crate maidsafe_utilities;
+extern crate rustc_serialize;
+#[macro_use]
+extern crate unwrap;
+
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use mio::{Ready, PollOpt, Poll, Token, Events};
+use mio::channel::{self, Sender};
+use mio::tcp::TcpListener;
+use std::time::Duration;
+use std::str::FromStr;
+use socket::Socket;
+use maidsafe_utilities::thread::{named as named_thread, Joiner};
+use std::io::{self, Cursor, Read, Write};
+use std::net::{SocketAddr, TcpStream};
+use std::mem;
+use std::sync::mpsc::{self, Receiver};
+use std::thread;
+use rustc_serialize::Decodable;
+use maidsafe_utilities::serialisation::{serialise, deserialise};
+
+mod socket;
+
+const MSG_TOKEN: usize = 0;
+const LISTENER_TOKEN: usize = MSG_TOKEN + 1;
+const SESSION_TOKEN: usize = LISTENER_TOKEN + 1;
+
+struct CoreMsg(Option<Box<FnMut(&Poll) + 'static + Send>>);
+impl CoreMsg {
+    fn _new<F: FnOnce(&Poll) + 'static + Send>(f: F) -> Self {
+        let mut f = Some(f);
+        CoreMsg(Some(Box::new(move |p: &Poll| {
+            let f = f.take().unwrap();
+            f(p)
+        })))
+    }
+
+    fn build_terminator() -> Self {
+        CoreMsg(None)
+    }
+}
+
+struct El {
+    tx: Sender<CoreMsg>,
+    listener_started_rx: Receiver<()>,
+    _joiner: Joiner,
+}
+impl Drop for El {
+    fn drop(&mut self) {
+        let _ = self.tx.send(CoreMsg::build_terminator());
+    }
+}
+
+struct Server {
+    listener: TcpListener,
+    session: Option<Session>,
+}
+
+struct Session(Socket);
+impl Session {
+    fn ready(&mut self, poll: &Poll, kind: Ready) {
+        if kind.is_error() || kind.is_hup() {
+            return;
+        } else if kind.is_readable() {
+            let _data = match self.0.read::<Vec<u8>>() {
+                Ok(Some(msg)) => msg,
+                Err(e) => panic!("Error in read {:?}", e),
+                Ok(None) => return,
+            };
+            let _ = self.0.write(poll, Token(SESSION_TOKEN), Some(vec![2u8, 3, 4, 5, 6]));
+        } else if kind.is_writable() {
+            let _ = self.0.write(poll, Token(SESSION_TOKEN), Option::None::<Vec<u8>>);
+        } else {
+            println!("Session socket: {:?}", kind);
+        }
+    }
+}
+
+fn spawn_el() -> El {
+    let (tx, rx) = channel::channel::<CoreMsg>();
+    let (listener_started_tx, listener_started_rx) = mpsc::channel();
+
+    let joiner = named_thread("Event Loop", move || {
+        let poll = unwrap!(Poll::new());
+        let mut events = Events::with_capacity(1024);
+
+        unwrap!(poll.register(&rx,
+                              Token(MSG_TOKEN),
+                              Ready::readable() | Ready::error() | Ready::hup(),
+                              PollOpt::edge()));
+
+        let listener = unwrap!(TcpListener::bind(&unwrap!(SocketAddr::from_str("0.0.0.0:5000"))));
+        unwrap!(poll.register(&listener,
+                              Token(LISTENER_TOKEN),
+                              Ready::readable() | Ready::error() | Ready::hup(),
+                              PollOpt::edge()));
+
+        let mut server = Server {
+            listener: listener,
+            session: None,
+        };
+
+        unwrap!(listener_started_tx.send(()));
+
+        'event_loop: loop {
+            unwrap!(poll.poll(&mut events, None), "Failed to Poll");
+
+            for event in events.iter() {
+                println!("{:?}", event);
+                match event.token() {
+                    Token(t) if t == LISTENER_TOKEN => {
+                        loop {
+                            let (peer, peer_addr) = match server.listener.accept() {
+                                Ok((peer, peer_addr)) => (Socket::wrap(peer), peer_addr),
+                                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock ||
+                                              e.kind() == io::ErrorKind::Interrupted => {
+                                    println!("Ignoring error in listening: {:?}", e);
+                                    break;
+                                }
+                                Err(e) => panic!("Error in listening: {:?}", e),
+                            };
+
+                            unwrap!(poll.register(&peer,
+                                                  Token(SESSION_TOKEN),
+                                                  Ready::readable() | Ready::error() |
+                                                  Ready::hup(),
+                                                  PollOpt::edge()));
+                            server.session = Some(Session(peer));
+
+                            println!("Accepted from peer: {:?}", peer_addr);
+                        }
+                    }
+                    Token(t) if t == SESSION_TOKEN => {
+                        unwrap!(server.session.as_mut()).ready(&poll, event.kind());
+                    }
+                    Token(t) if t == MSG_TOKEN => {
+                        let msg = unwrap!(rx.try_recv());
+                        if let Some(mut f) = msg.0 {
+                            f(&poll);
+                        } else {
+                            break 'event_loop;
+                        }
+                    }
+                    x => panic!("Unexpected token: {:?}", x),
+                }
+            }
+        }
+
+        println!("Exiting Event loop");
+    });
+
+    El {
+        tx: tx,
+        listener_started_rx: listener_started_rx,
+        _joiner: joiner,
+    }
+}
+
+fn sync_write(stream: &mut TcpStream, message: Vec<u8>) {
+    let mut size_vec = Vec::with_capacity(mem::size_of::<u32>());
+    unwrap!(size_vec.write_u32::<LittleEndian>(message.len() as u32));
+
+    unwrap!(stream.write_all(&size_vec));
+    unwrap!(stream.write_all(&message));
+}
+
+#[allow(unsafe_code)]
+fn sync_read<T: Decodable>(stream: &mut TcpStream) -> T {
+    let mut payload_size_buffer = [0; 4];
+    unwrap!(stream.read_exact(&mut payload_size_buffer));
+
+    let payload_size = unwrap!(Cursor::new(&payload_size_buffer[..])
+        .read_u32::<LittleEndian>()) as usize;
+
+    let mut payload = Vec::with_capacity(payload_size);
+    unsafe {
+        payload.set_len(payload_size);
+    }
+    unwrap!(stream.read_exact(&mut payload));
+
+    unwrap!(deserialise(&payload), "Could not deserialise.")
+}
+
+fn main() {
+    let el = spawn_el();
+    unwrap!(el.listener_started_rx.recv());
+    let mut us = unwrap!(TcpStream::connect("127.0.0.1:5000"));
+    sync_write(&mut us, unwrap!(serialise(&vec![1, 2, 3, 4, 60, 97])));
+    let blocking_rx = sync_read::<Vec<u8>>(&mut us);
+    println!("Blocking Rxd from peer: {:?}", blocking_rx);
+    sync_write(&mut us,
+               unwrap!(serialise(&vec![11, 21, 33, 41, 160, 99, 233])));
+    println!("Written - If windows has gone into wouldblock then this will panic...");
+    thread::sleep(Duration::from_secs(10));
+}
---
+++ src/socket.rs
@@ -0,0 +1,251 @@
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use maidsafe_utilities::serialisation::{deserialise_from, serialise_into};
+use mio::{Evented, Poll, PollOpt, Ready, Token};
+use mio::tcp::TcpStream;
+use rustc_serialize::{Decodable, Encodable};
+use std::collections::VecDeque;
+use std::io::{self, Cursor, ErrorKind, Read, Write};
+use std::mem;
+use std::net::SocketAddr;
+
+pub struct Socket {
+    inner: Option<SockInner>,
+}
+
+impl Socket {
+    pub fn _connect(addr: &SocketAddr) -> io::Result<Self> {
+        let stream = TcpStream::connect(addr)?;
+        Ok(Self::wrap(stream))
+    }
+
+    pub fn wrap(stream: TcpStream) -> Self {
+        Socket {
+            inner: Some(SockInner {
+                stream: stream,
+                read_buffer: Vec::new(),
+                read_len: 0,
+                write_queue: VecDeque::new(),
+                current_write: None,
+            }),
+        }
+    }
+
+    pub fn _peer_addr(&self) -> io::Result<SocketAddr> {
+        let inner = unwrap!(self.inner.as_ref());
+        Ok(inner.stream.peer_addr()?)
+    }
+
+    // Read message from the socket. Call this from inside the `ready` handler.
+    //
+    // Returns:
+    //   - Ok(Some(data)): data has been successfuly read from the socket
+    //   - Ok(None):       there is not enough data in the socket. Call `read`
+    //                     again in the next invocation of the `ready` handler.
+    //   - Err(error):     there was an error reading from the socket.
+    pub fn read<T: Decodable>(&mut self) -> io::Result<Option<T>> {
+        let inner = unwrap!(self.inner.as_mut());
+        inner.read()
+    }
+
+    // Write a message to the socket.
+    //
+    // Returns:
+    //   - Ok(true):   the message has been successfuly written.
+    //   - Ok(false):  the message has been queued, but not yet fully written.
+    //                 Write event is already scheduled for next time.
+    //   - Err(error): there was an error while writing to the socket.
+    pub fn write<T: Encodable>(&mut self,
+                               poll: &Poll,
+                               token: Token,
+                               msg: Option<T>)
+                               -> io::Result<bool> {
+        let inner = unwrap!(self.inner.as_mut());
+        inner.write(poll, token, msg)
+    }
+}
+
+impl Default for Socket {
+    fn default() -> Self {
+        Socket { inner: None }
+    }
+}
+
+impl Evented for Socket {
+    fn register(&self,
+                poll: &Poll,
+                token: Token,
+                interest: Ready,
+                opts: PollOpt)
+                -> io::Result<()> {
+        let inner = unwrap!(self.inner.as_ref());
+        inner.register(poll, token, interest, opts)
+    }
+
+    fn reregister(&self,
+                  poll: &Poll,
+                  token: Token,
+                  interest: Ready,
+                  opts: PollOpt)
+                  -> io::Result<()> {
+        let inner = unwrap!(self.inner.as_ref());
+        inner.reregister(poll, token, interest, opts)
+    }
+
+    fn deregister(&self, poll: &Poll) -> io::Result<()> {
+        let inner = unwrap!(self.inner.as_ref());
+        inner.deregister(poll)
+    }
+}
+
+struct SockInner {
+    stream: TcpStream,
+    read_buffer: Vec<u8>,
+    read_len: usize,
+    write_queue: VecDeque<Vec<u8>>,
+    current_write: Option<Vec<u8>>,
+}
+
+impl SockInner {
+    // Read message from the socket. Call this from inside the `ready` handler.
+    //
+    // Returns:
+    //   - Ok(Some(data)): data has been successfuly read from the socket
+    //   - Ok(None):       there is not enough data in the socket. Call `read`
+    //                     again in the next invocation of the `ready` handler.
+    //   - Err(error):     there was an error reading from the socket.
+    fn read<T: Decodable>(&mut self) -> io::Result<Option<T>> {
+        if let Some(message) = self.read_from_buffer()? {
+            return Ok(Some(message));
+        }
+
+        // the mio reading window is max at 64k (64 * 1024)
+        let mut buffer = [0; 65536];
+
+        match self.stream.read(&mut buffer) {
+            Ok(bytes_read) => {
+                self.read_buffer.extend_from_slice(&buffer[0..bytes_read]);
+                self.read_from_buffer()
+            }
+
+            Err(error) => {
+                if error.kind() == ErrorKind::WouldBlock || error.kind() == ErrorKind::Interrupted {
+                    println!("================== Error in read {:?}", error.kind());
+                    Ok(None)
+                } else {
+                    Err(error)
+                }
+            }
+        }
+    }
+
+    fn read_from_buffer<T: Decodable>(&mut self) -> io::Result<Option<T>> {
+        let u32_size = mem::size_of::<u32>();
+
+        if self.read_len == 0 {
+            if self.read_buffer.len() < u32_size {
+                return Ok(None);
+            }
+
+            self.read_len = Cursor::new(&self.read_buffer).read_u32::<LittleEndian>()? as usize;
+
+            self.read_buffer = self.read_buffer[u32_size..].to_owned();
+        }
+
+        if self.read_len > self.read_buffer.len() {
+            return Ok(None);
+        }
+
+        let result = unwrap!(deserialise_from(&mut Cursor::new(&self.read_buffer)));
+
+        self.read_buffer = self.read_buffer[self.read_len..].to_owned();
+        self.read_len = 0;
+
+        Ok(Some(result))
+    }
+
+    // Write a message to the socket.
+    //
+    // Returns:
+    //   - Ok(true):   the message has been successfuly written.
+    //   - Ok(false):  the message has been queued, but not yet fully written.
+    //                 Write event is already scheduled for next time.
+    //   - Err(error): there was an error while writing to the socket.
+    fn write<T: Encodable>(&mut self,
+                           poll: &Poll,
+                           token: Token,
+                           msg: Option<T>)
+                           -> io::Result<bool> {
+        if let Some(msg) = msg {
+            let mut data = Cursor::new(Vec::with_capacity(mem::size_of::<u32>()));
+
+            let _ = data.write_u32::<LittleEndian>(0);
+
+            unwrap!(serialise_into(&msg, &mut data));
+
+            let len = data.position() - mem::size_of::<u32>() as u64;
+            data.set_position(0);
+            data.write_u32::<LittleEndian>(len as u32)?;
+
+            self.write_queue.push_back(data.into_inner());
+        }
+
+        if self.current_write.is_none() {
+            let data = unwrap!(self.write_queue.pop_front());
+            self.current_write = Some(data);
+        }
+
+        if let Some(data) = self.current_write.take() {
+            match self.stream.write(&data) {
+                Ok(bytes_txd) => {
+                    if bytes_txd < data.len() {
+                        self.current_write = Some(data[bytes_txd..].to_owned());
+                    }
+                }
+                Err(error) => {
+                    if error.kind() == ErrorKind::WouldBlock ||
+                       error.kind() == ErrorKind::Interrupted {
+                        self.current_write = Some(data);
+                    } else {
+                        return Err(From::from(error));
+                    }
+                }
+            }
+        }
+
+        let done = self.current_write.is_none() && self.write_queue.is_empty();
+
+        let event_set = if done {
+            Ready::error() | Ready::hup() | Ready::readable()
+        } else {
+            Ready::error() | Ready::hup() | Ready::readable() | Ready::writable()
+        };
+
+        poll.reregister(self, token, event_set, PollOpt::edge())?;
+
+        Ok(done)
+    }
+}
+
+impl Evented for SockInner {
+    fn register(&self,
+                poll: &Poll,
+                token: Token,
+                interest: Ready,
+                opts: PollOpt)
+                -> io::Result<()> {
+        self.stream.register(poll, token, interest, opts)
+    }
+
+    fn reregister(&self,
+                  poll: &Poll,
+                  token: Token,
+                  interest: Ready,
+                  opts: PollOpt)
+                  -> io::Result<()> {
+        self.stream.reregister(poll, token, interest, opts)
+    }
+
+    fn deregister(&self, poll: &Poll) -> io::Result<()> {
+        self.stream.deregister(poll)
+    }
+}
